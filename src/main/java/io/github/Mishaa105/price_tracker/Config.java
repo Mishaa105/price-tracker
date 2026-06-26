dbb13e2d809a7c0376beea7862f8a6cf36326738
“Труханов <kurkanos2006@gmail.com>
2026-06-26 19:10:44 +0300

    Initial commit with first version of html loader

---
+++ src/main/java/io/github/Mishaa105/price_tracker/Config.java
@@ -0,0 +1,13 @@
+package io.github.Mishaa105.price_tracker;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+@Validated
+@ConfigurationProperties(prefix = "app")
+public record Config(@NotNull Playstation playstation)
+{
+    public record Playstation (@NotBlank String baseUrl) {}
+}
